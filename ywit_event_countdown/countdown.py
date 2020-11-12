8a672811c7374243c99a6c0c5e84b60c294c4785
Robert Blackhart <robert@robertblackhart.com>
2020-11-12 11:28:38 -0500

    change TZ behavior to make more universal

--- ywit_event_countdown/countdown.py
+++ ywit_event_countdown/countdown.py
@@ -3,7 +3,7 @@ This is the main functionality of our package. it implements the logic necessary
 to fetch the next event from the server and return it as a time delta for us to use.
 """
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import requests
 
@@ -17,7 +17,7 @@ def get_time_difference(from_time: datetime = None) -> timedelta:
     if not from_time:
         from_time = datetime.now()
     if not from_time.tzinfo:
-        from_time = from_time.replace(tzinfo=datetime.now().astimezone().tzinfo)
+        from_time = from_time.replace(tzinfo=timezone.utc)
 
     next_ywit_event = datetime.fromisoformat(_get_event_from_api())
     return next_ywit_event - from_time
